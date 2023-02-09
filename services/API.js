f13fc5d68de652ea6e9a9aea37c6ea25a3408e06
Menamilochomil <jimenamilochomil@gmail.com>
2023-02-08 19:57:08 -0600

    feat: get data from APIs and render in the screen

--- services/API.js
+++ services/API.js
@@ -1,1 +1,22 @@
-const BASE_URL = 'https://search.reservamos.mx/api/v2/places?q';
+module.exports = () => {
+    let data = []
+  let searchName = document.getElementById("search-city").value;
+
+  const requestOptions = {
+    method: 'GET',
+    redirect: 'follow'
+  };
+  const URL = "https://search.reservamos.mx/api/v2/places?q="
+
+//Replace monterrey por una variable de lo que entra en searchName
+  fetch( URL + "monterrey", requestOptions)
+    .then(response => response.json())
+    .then(result => console.log(data = {
+      city: result[0].city_name,
+      lat: result[0].lat,
+      long: result[0].long
+    }))
+    .catch(error => console.log('error', error));
+
+  return data
+}
--- src/index.js
+++ src/index.js
@@ -1,19 +1,38 @@
 //Getting the lat and long from reservamos API
-let requestOptions = {
-    method: 'GET',
-    redirect: 'follow'
-  };
 
-let data = []
-//Replace monterrey por una variable de lo que entra en search
-  fetch("https://search.reservamos.mx/api/v2/places?q=monterrey", requestOptions)
+// let searchName = document.getElementById("search-city").value;
+
+const requestOptions = {
+  method: 'GET',
+  redirect: 'follow'
+};
+const URL = "https://search.reservamos.mx/api/v2/places?q="
+
+//Replace monterrey por una variable de lo que entra en searchName
+fetch(URL + "monterrey", requestOptions)
+
+  .then(response => response.json())
+  .then(data => getData(data))
+  .catch(error => console.log('error', error));
+
+  //Getting daily weather data from openweather API
+const getData = (data) => {
+  console.log("soy monterrey", data)
+  let lat = data[0].lat;
+  let long = data[0].long;
+  document.getElementById("current-name").innerText = "Weather in " + data[0].display
+
+  fetch('https://api.openweathermap.org/data/2.5/onecall?lat=' + lat +
+    '&lon=' + long + '&exclude=hourly,minutely&units=metric&appid=a5a47c18197737e8eeca634cd6acb581')
     .then(response => response.json())
-    .then(result => console.log(data ={
-      city:result[0].city_name,
-      lat: result[0].lat,
-      long: result[0].long
-    }))
-    .catch(error => console.log('error', error));
+    .then(data => {
 
-//Getting the weather from openweathermap API
-// console.log(arr[0].city)
+      document.getElementById("current-temp").innerText = "Tempeture " + data.current.temp + "°"
+      document.getElementById("current-humidity").innerText = "Humidity " + data.current.humidity + "%"
+      document.getElementById("current-icon").src ="https://openweathermap.org/img/wn/" + data.current.weather[0].icon + ".png";
+      document.getElementById("current-description").innerText = data.current.weather[0].description
+
+    console.log("soy clima", data)
+
+})
+}
